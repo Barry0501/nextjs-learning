ad9585fb829b833b255ad43bcd9b4f5bbc8b4cb8
Tùng Đường <>
2024-06-09 23:58:17 +0700

    Add default dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import { Button } from "@nextui-org/button";
 import { NextUIProvider } from "@nextui-org/react";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <NextUIProvider>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center justify-between p-24 dark text-foreground bg-background">
         <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
           <Link href="/dashboard">
             <h2 className="mb-3 text-2xl font-semibold">
@@ -18,7 +17,6 @@ export default function Home() {
               Find in-depth information about Next.js features and API.
             </p>
           </Link>
-          <Button>Press me</Button>
         </div>
       </main>
     </NextUIProvider>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,10 @@ const config: Config = {
       },
     },
   },
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      defaultTheme: "dark",
+    }),
+  ],
 };
 export default config;
