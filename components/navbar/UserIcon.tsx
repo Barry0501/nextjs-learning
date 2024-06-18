e13f91294f4b946311caea2ff902307203d7e2ad
Tùng Đường <>
2024-06-18 12:00:55 +0700

    Fetch User image

--- components/navbar/UserIcon.tsx
+++ components/navbar/UserIcon.tsx
@@ -1,6 +1,20 @@
+import { fetchProfileImage } from "@/utils/actions";
+import Image from "next/image";
 import { LuUser2 } from "react-icons/lu";
 
-function UserIcon() {
+async function UserIcon() {
+  const profileImage = await fetchProfileImage();
+
+  if (profileImage)
+    return (
+      <Image
+        src={profileImage}
+        className="w-6 h-6 rounded-full object-cover"
+        alt="user avatar"
+        width={24}
+        height={24}
+      />
+    );
   return <LuUser2 className="w-6 h-6 bg-primary rounded-full text-white" />;
 }
 
