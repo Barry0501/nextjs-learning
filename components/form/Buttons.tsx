b7183cb52dafde1a76722dd7f4c97d06435a8770
Tùng Đường <>
2024-06-18 13:28:17 +0700

    Input Image Container

--- components/form/Buttons.tsx
+++ components/form/Buttons.tsx
@@ -1,44 +1,11 @@
 "use client";
 
-import { ReloadIcon } from "@radix-ui/react-icons";
-import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { SignInButton } from "@clerk/nextjs";
-import { FaRegHeart, FaHeart } from "react-icons/fa";
-import { LuTrash2, LuPenSquare } from "react-icons/lu";
-type btnSize = "default" | "lg" | "sm";
-
-type SubmitButtonProps = {
-  className?: string;
-  text?: string;
-  size?: btnSize;
-};
-
-export function SubmitButton({
-  className = "",
-  text = "submit",
-  size = "lg",
-}: SubmitButtonProps) {
-  const { pending } = useFormStatus();
-
-  return (
-    <Button
-      type="submit"
-      disabled={pending}
-      className={`capitalize ${className} `}
-      size={size}
-    >
-      {pending ? (
-        <>
-          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-          Please wait...
-        </>
-      ) : (
-        text
-      )}
-    </Button>
-  );
-}
+import { ReloadIcon } from "@radix-ui/react-icons";
+import { useFormStatus } from "react-dom";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { LuPenSquare, LuTrash2 } from "react-icons/lu";
 
 export const CardSignInButton = () => {
   return (
